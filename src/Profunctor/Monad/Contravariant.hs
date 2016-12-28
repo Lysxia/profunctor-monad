879296b351771cf7f4252d3f4b6c3c96142c0123
lyxia <lysxia@gmail.com>
2016-12-28 16:33:44 +0000

    Describe laws for Contravariant

--- src/Profunctor/Monad/Contravariant.hs
+++ src/Profunctor/Monad/Contravariant.hs
@@ -15,11 +15,50 @@ infixl 5 =:, =.
 -- | Types @p :: * -> * -> *@ which are contravariant functors
 -- over their first parameter.
 --
+-- Functor law:
+--
 -- @
--- (f >>> g) `lmap` p
+-- lmap (i >>> j) p
 -- =
--- f `lmap` (g `lmap` p)
+-- lmap i (lmap j p)
 -- @
+--
+-- If for every type @a@, @p a@ is an instance of 'Functor' (i.e., @p@ is a
+-- covariant functor over its second parameter in Hask),
+-- then 'lmap' should commute with 'fmap' (thus @p@ is a bifunctor):
+--
+-- @
+-- lmap i (fmap f p)
+-- =
+-- fmap f (lmap i p)
+-- @
+--
+-- If the domain @First p@ is an 'Arrow', and if for every @a@, @p a@ is an
+-- instance of 'Applicative', then a pure arrow 'arr f' should correspond to
+-- an "applicative natural transformation":
+--
+-- @
+-- lmap (arr f) (p <*> q)
+-- =
+-- lmap (arr f) p <*> lmap (arr f) q
+-- @
+--
+-- @
+-- lmap (arr f) (pure a)
+-- =
+-- pure a
+-- @
+--
+-- The following may not be true in general, but seems to hold in practice,
+-- when the instance @'Applicative' (p a)@ orders effects from left to right,
+-- in particular that should be the case if there is also a @'Monad' (p a)@:
+--
+-- @
+-- lmap (first i) (lmap (arr fst) p <*> lmap (arr snd) q)
+-- =
+-- lmap (first i >>> arr fst) p <*> lmap (arr snd) q
+-- @
+--
 class Category (First p) => Contravariant p where
   type First p :: * -> * -> *
   lmap :: First p y x -> p x a -> p y a
