c0fed0d84a10e837053b4d2a0b300bbdee546be0
lyxia <lysxia@gmail.com>
2016-12-29 12:25:30 +0000

    Add Profunctor.Monad.Syntax

---
+++ src/Profunctor/Monad/Syntax.hs
@@ -0,0 +1,79 @@
+-- | Rebind syntax locally.
+--
+-- @
+-- {-# LANGUAGE RebindableSyntax #-}
+--
+-- let Syntax{..} = promonadSyntax
+-- in do
+--   ...
+-- @
+
+{-# LANGUAGE RankNTypes #-}
+
+module Profunctor.Monad.Syntax where
+
+import Data.Constraint.Forall (ForallF)
+import Control.Monad.Fail (MonadFail)
+import qualified Profunctor.Monad.Combinators as PM
+import Prelude (String, Functor, Applicative, Monad)
+
+data Syntax = Syntax
+  { (<$>)
+      :: forall p x a b
+      . ForallF Functor p
+      => (a -> b) -> p x a -> p x b
+
+  , (<*>)
+      :: forall p x a b
+      .  ForallF Applicative p
+      => p x (a -> b) -> p x a -> p x b
+
+  , pure
+      :: forall p x a
+      .  ForallF Applicative p
+      => a -> p x a
+
+  , (<*)
+      :: forall p x a b
+      .  ForallF Applicative p
+      => p x a -> p x b -> p x a
+
+  , (*>)
+      :: forall p x a b
+      .  ForallF Applicative p
+      => p x a -> p x b -> p x b
+
+  , (>>=)
+      :: forall p x a b
+      .  ForallF Monad p
+      => p x a -> (a -> p x b) -> p x b
+
+  , (>>)
+      :: forall p x a b
+      .  ForallF Monad p
+      => p x a -> p x b -> p x b
+
+  , return
+      :: forall p x a
+      .  ForallF Monad p
+      => a -> p x a
+
+  , fail
+      :: forall p x a
+      .  ForallF MonadFail p
+      => String -> p x a
+
+  }
+
+promonad :: Syntax
+promonad = Syntax
+  (PM.<$>)
+  (PM.<*>)
+  PM.pure
+  (PM.<*)
+  (PM.*>)
+  (PM.>>=)
+  (PM.>>)
+  PM.return
+  PM.fail
+
