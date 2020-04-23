673c4cea888224f0f6b4dccf3836229f50be56c8
Lysxia <lysxia@gmail.com>
2020-04-23 09:10:00 -0400

    Tweaks

--- src/Profunctor/Monad/NewCombinators.hs
+++ src/Profunctor/Monad/NewCombinators.hs
@@ -36,63 +36,30 @@ tailMaybe :: [a] -> Maybe [a]
 tailMaybe [] = Nothing
 tailMaybe (_ : xs) = Just xs
 
-{-
-
 manyP
-  :: forall p x a
-  .  (Profunctor p, ForallF Alternative p)
+  :: (ProfunctorPartial p, Alternative (p [x]))
   => (([x] -> Bool) -> p [x] ()) -> p x a -> p [x] [a]
-manyP = with @Alternative @p @[x] manyP_
-
-manyP_
-  :: (Profunctor p, Alternative (p [x]))
-  => (([x] -> Bool) -> p [x] ()) -> p x a -> p [x] [a]
-manyP_ assert p = someP_ assert p <|> pure []
+manyP assert p = someP assert p <|> pure []
 
 someP
-  :: forall p x a
-  .  (Profunctor p, ForallF Alternative p)
+  :: (ProfunctorPartial p, Alternative (p [x]))
   => (([x] -> Bool) -> p [x] ()) -> p x a -> p [x] [a]
-someP = with @Alternative @p @[x] someP_
-
-someP_
-  :: (Profunctor p, Alternative (p [x]))
-  => (([x] -> Bool) -> p [x] ()) -> p x a -> p [x] [a]
-someP_ assert p =
-  assert (not . null) *> liftA2 (:) (head =. p) (tail =. manyP_ assert p)
+someP assert p =
+  assert (not . null) *> liftA2 (:) (headMaybe `comap` p) (tailMaybe `comap` manyP assert p)
 
 sepByP
-  :: forall p x a b
-  .  (Profunctor p, ForallF Alternative p)
+  :: (ProfunctorPartial p, Alternative (p [x]))
   => (([x] -> Bool) -> p [x] ()) -> p x a -> p () b -> p [x] [a]
-sepByP = with @Alternative @p @[x] sepByP_
-
-sepByP_
-  :: (Profunctor p, Alternative (p [x]))
-  => (([x] -> Bool) -> p [x] ()) -> p x a -> p () b -> p [x] [a]
-sepByP_ assert p s =
-  (assert (not . null) *> sepBy1P_ assert p s) <|> pure []
+sepByP assert p s =
+  (assert (not . null) *> sepBy1P assert p s) <|> pure []
 
 sepBy1P
-  :: forall p x a b
-  .  (Profunctor p, ForallF Alternative p)
+  :: (ProfunctorPartial p, Alternative (p [x]))
   => (([x] -> Bool) -> p [x] ()) -> p x a -> p () b -> p [x] [a]
-sepBy1P = with @Alternative @p @[x] sepBy1P_
-
-sepBy1P_
-  :: (Profunctor p, Alternative (p [x]))
-  => (([x] -> Bool) -> p [x] ()) -> p x a -> p () b -> p [x] [a]
-sepBy1P_ assert p s = liftA2 (:) (head =. p) (tail =. preByP_ assert p s)
+sepBy1P assert p s = liftA2 (:) (headMaybe `comap` p) (tailMaybe `comap` preByP assert p s)
 
 preByP
-  :: forall p x a b
-  .  (Profunctor p, ForallF Alternative p)
+  :: (ProfunctorPartial p, Alternative (p [x]))
   => (([x] -> Bool) -> p [x] ()) -> p x a -> p () b -> p [x] [a]
-preByP = with @Alternative @p @[x] preByP_
-
-preByP_
-  :: (Profunctor p, Alternative (p [x]))
-  => (([x] -> Bool) -> p [x] ()) -> p x a -> p () b -> p [x] [a]
-preByP_ assert p s =
-  (assert (not . null) *> const () =. s *> sepBy1P_ assert p s) <|> pure []
--}
+preByP assert p s =
+  (assert (not . null) *> const () `lmap` s *> sepBy1P assert p s) <|> pure []
--- src/Profunctor/Monad/Partial.hs
+++ src/Profunctor/Monad/Partial.hs
@@ -4,6 +4,9 @@ module Profunctor.Monad.Partial
   ( ProfunctorPartial(..)
   , comap
   , upon
+  , upon'
+  , (=.)
+  , (=.?)
   , J
 
     -- * Reexported from profunctors
@@ -20,9 +23,23 @@ class Profunctor p => ProfunctorPartial p where
 comap :: ProfunctorPartial p => (a -> Maybe a') -> p a' b -> p a b
 comap f = lmap f . cofail
 
--- | Flipped version of 'comap'.
+-- | Flipped version of 'comap'. Partial variant of 'upon''.
 upon :: ProfunctorPartial p => p a' b -> (a -> Maybe a') -> p a b
 upon = flip comap
+
+-- | Flipped version of 'lmap'. Total variant of 'upon'.
+upon' :: ProfunctorPartial p => p a' b -> (a -> a') -> p a b
+upon' = flip lmap
+
+infixl 7 `upon`, `upon'`
+
+(=.) :: Profunctor p => (y -> x) -> p x a -> p y a
+(=.) = lmap
+
+infixr 5 =., =.?
+
+(=.?) :: ProfunctorPartial p => (y -> Maybe x) -> p x a -> p y a
+(=.?) = comap
 
 -- | Abbreviation for types of the form @p a a@.
 -- Named 'J' because this is the 'join' of the reader monad @(->)@.
